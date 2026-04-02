f5625d966497b16853593b66a60405fcee4522ee
Otaksi-Clients <otaksiclients@gmail.com>
2026-04-02 08:17:48 +0400

    Replace social emojis with inline SVG icons
    Replace emoji icons in the socialLinks array with inline SVG JSX for LinkedIn, Twitter, GitHub, Instagram, and YouTube. Preserves existing href and gradient color fields while adding viewBox, fill, className and aria-hidden attributes for consistent sizing and accessibility.

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -35,11 +35,56 @@ const resourcesLinks = [
 ]
 
 const socialLinks = [
-  { name: 'LinkedIn', icon: '💼', href: 'https://linkedin.com/company/otaksi-connect', color: 'from-[#0077B5] to-[#00A0DC]' },
-  { name: 'Twitter', icon: '🐦', href: 'https://twitter.com/otaksiconnect', color: 'from-[#1DA1F2] to-[#1A91DA]' },
-  { name: 'GitHub', icon: '🐙', href: 'https://github.com/otaksi-connect', color: 'from-[#333] to-[#6e5494]' },
-  { name: 'Instagram', icon: '📷', href: 'https://instagram.com/otaksiconnect', color: 'from-[#E4405F] to-[#D52B5C]' },
-  { name: 'YouTube', icon: '▶️', href: 'https://youtube.com/@otaksiconnect', color: 'from-[#FF0000] to-[#CC0000]' },
+  {
+    name: 'LinkedIn',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" aria-hidden="true">
+        <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm15.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z" />
+      </svg>
+    ),
+    href: 'https://linkedin.com/company/otaksi-connect',
+    color: 'from-[#0077B5] to-[#00A0DC]'
+  },
+  {
+    name: 'Twitter',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" aria-hidden="true">
+        <path d="M22.46 5.92c-.8.36-1.66.6-2.56.71a4.48 4.48 0 0 0 1.97-2.48 8.94 8.94 0 0 1-2.83 1.08 4.48 4.48 0 0 0-7.63 4.08A12.72 12.72 0 0 1 3.11 4.6a4.48 4.48 0 0 0 1.39 5.98c-.73-.02-1.42-.22-2.02-.56v.06a4.48 4.48 0 0 0 3.6 4.39c-.34.09-.7.14-1.07.14-.26 0-.51-.02-.76-.07a4.48 4.48 0 0 0 4.18 3.11A8.98 8.98 0 0 1 2 19.54a12.7 12.7 0 0 0 6.88 2.02c8.26 0 12.78-6.84 12.78-12.78 0-.2 0-.39-.01-.59a9.1 9.1 0 0 0 2.24-2.32z" />
+      </svg>
+    ),
+    href: 'https://twitter.com/otaksiconnect',
+    color: 'from-[#1DA1F2] to-[#1A91DA]'
+  },
+  {
+    name: 'GitHub',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" aria-hidden="true">
+        <path d="M12 2C6.48 2 2 6.58 2 12.26c0 4.49 2.87 8.3 6.84 9.64.5.09.68-.22.68-.48 0-.24-.01-.87-.01-1.7-2.78.62-3.37-1.36-3.37-1.36-.45-1.18-1.1-1.5-1.1-1.5-.9-.63.07-.62.07-.62 1 .07 1.53 1.05 1.53 1.05.89 1.56 2.34 1.11 2.91.85.09-.66.35-1.11.63-1.37-2.22-.26-4.56-1.14-4.56-5.07 0-1.12.39-2.03 1.03-2.75-.1-.26-.45-1.3.1-2.7 0 0 .84-.28 2.75 1.05a9.36 9.36 0 0 1 2.5-.34c.85 0 1.7.11 2.5.34 1.91-1.33 2.75-1.05 2.75-1.05.55 1.4.2 2.44.1 2.7.64.72 1.03 1.63 1.03 2.75 0 3.94-2.34 4.81-4.57 5.07.36.32.68.94.68 1.9 0 1.37-.01 2.47-.01 2.81 0 .27.18.58.69.48A10.01 10.01 0 0 0 22 12.26C22 6.58 17.52 2 12 2z" />
+      </svg>
+    ),
+    href: 'https://github.com/otaksi-connect',
+    color: 'from-[#333] to-[#6e5494]'
+  },
+  {
+    name: 'Instagram',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" aria-hidden="true">
+        <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm4.25 3.25a5.25 5.25 0 1 1 0 10.5 5.25 5.25 0 0 1 0-10.5zm0 1.5a3.75 3.75 0 1 0 0 7.5 3.75 3.75 0 0 0 0-7.5zm5.38 1.13a1.13 1.13 0 1 1-2.25 0 1.13 1.13 0 0 1 2.25 0z" />
+      </svg>
+    ),
+    href: 'https://instagram.com/otaksiconnect',
+    color: 'from-[#E4405F] to-[#D52B5C]'
+  },
+  {
+    name: 'YouTube',
+    icon: (
+      <svg viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6" aria-hidden="true">
+        <path d="M21.8 8.001a2.75 2.75 0 0 0-1.94-1.94C18.2 6 12 6 12 6s-6.2 0-7.86.06a2.75 2.75 0 0 0-1.94 1.94C2 9.661 2 12.001 2 12.001s0 2.34.2 3.999a2.75 2.75 0 0 0 1.94 1.94C5.8 18 12 18 12 18s6.2 0 7.86-.06a2.75 2.75 0 0 0 1.94-1.94C22 14.341 22 12.001 22 12.001s0-2.34-.2-3.999zM10 15.5v-7l6 3.5-6 3.5z" />
+      </svg>
+    ),
+    href: 'https://youtube.com/@otaksiconnect',
+    color: 'from-[#FF0000] to-[#CC0000]'
+  },
 ]
 
 const contactInfo = [
